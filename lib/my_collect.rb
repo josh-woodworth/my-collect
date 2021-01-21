2f456542a4f74aedde2f789991cfbd4cacbec6f8
Josh Woodworth <72992058+josh-woodworth@users.noreply.github.com>
2021-01-20 22:18:21 -0700

    commit tree cc5c8da3972642b27aba2aef871922d739a0ecb3

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"] 
 
+def my_collect(array)
+    x = 0
+    collection = []
+    while x < array.count
+        collection << yield(array[x])
+        x += 1
+    end
+    collection
+end
+
+my_collect(array) {|name| name.split(" ").first}
 
